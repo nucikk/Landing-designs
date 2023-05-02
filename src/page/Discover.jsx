2ecf80bb04ef423153764b1916b597e43c3bb863
nuci <kirianuci@gmail.com>
2023-05-02 19:13:33 +0400

    add discover image section grid method

--- src/page/Discover.jsx
+++ src/page/Discover.jsx
@@ -1,6 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "../style/discover.css";
+import Rectangle1 from "../Image/Rectangle 1.png"
+import Rectangle2 from "../Image/Rectangle 2.png"
+import Rectangle3 from "../Image/Rectangle 3.png"
+import Rectangle4 from "../Image/Rectangle 4.png"
+import Rectangle5 from "../Image/Rectangle 5.png"
+import Rectangle6 from "../Image/Rectangle 6.png"
+import Rectangle7 from "../Image/Rectangle 7.png"
+import Rectangle8 from "../Image/Rectangle 8.png"
+
 
 const Discover = () => {
   return (
@@ -11,6 +20,19 @@ const Discover = () => {
          <p className="discover_description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis beatae illo a laborum, eligendi atque debitis excepturi neque, nostrum incidunt fuga.</p>
           <button className="discover_btn">Explore</button>
         </div>
+      </div>
+
+      <div className="discover_img_box">
+
+    <img src={Rectangle1}/> 
+    <img src={Rectangle2}/> 
+    <img src={Rectangle3}/> 
+    <img src={Rectangle4}/> 
+
+    <img src={Rectangle5}/> 
+    <img src={Rectangle6}/> 
+    <img src={Rectangle7}/> 
+    <img src={Rectangle8}/> 
       </div>
     </div>
   );
