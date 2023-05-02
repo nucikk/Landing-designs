72debc26d03191ea1cd1d56fd0bd2cf51075f7c1
nuci <kirianuci@gmail.com>
2023-05-02 16:23:16 +0400

    add description and styling Discover component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Discover from './page/Discover'
+
+function App() {
+ 
+  return (
+    <>
+      <Discover/>
+    </>
+  )
+}
+
+export default App
---
+++ src/page/Discover.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "../style/discover.css";
+
+const Discover = () => {
+  return (
+    <div className="discover">
+      <div className="discover_container">
+        <div className="discover_info">
+         <h2 className="discover_title">Discover the beauty around the world</h2>
+         <p className="discover_description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis beatae illo a laborum, eligendi atque debitis excepturi neque, nostrum incidunt fuga.</p>
+          <button className="discover_btn">Explore</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
